fd90d5f5c5fd3df85d4ae438fade450eb3b9a1a0
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-11-03 16:58:30 +0530

    ngfor in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-application';
 
-  name() {
-    let val = new FormData();
+  title = 'demo';
+  disablebutton:boolean = true;
+  values:string = " ";
 
+  Employees=[
+    {name:"dhanesh",designation:"web developer"},
+    {name:"vajagani",designation:"MERN developer"},
+    {name:"alex hales",designation:"MEAN developer"}
+
+
+  ]
+
+  printvalue(){
+    this.values = "hello dhanesh"
+    console.log("hello");
+    
   }
 }
